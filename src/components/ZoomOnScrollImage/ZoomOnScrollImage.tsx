e848e5241ddf6780d672251196c649ece739e3a6
Mihai Ursu <ursu.mihai.mail@gmail.com>
2022-11-22 21:22:50 +0200

    Finished the effect

--- src/components/ZoomOnScrollImage/ZoomOnScrollImage.tsx
+++ src/components/ZoomOnScrollImage/ZoomOnScrollImage.tsx
@@ -1,10 +1,17 @@
 import { motion } from "framer-motion";
+import { useRef } from "react";
+import useZoomOnScroll from "./hooks/useZoomOnScroll";
 import styles from "./ZoomOnScrollImage.module.scss";
 
 const ZoomOnScrollImage = () => {
+  const ref = useRef<HTMLDivElement>(null);
+
+  const scale = useZoomOnScroll(ref);
+
   return (
-    <div className={styles.wrapper}>
+    <div className={styles.wrapper} ref={ref}>
       <motion.div
+        style={{ scale: scale }}
         initial={{ scale: 0.4 }}
         className={styles.contentWrapper}
       ></motion.div>
--- src/components/ZoomOnScrollImage/hooks/useZoomOnScroll.ts
+++ src/components/ZoomOnScrollImage/hooks/useZoomOnScroll.ts
@@ -1,5 +1,34 @@
-import { RefObject } from "react";
+import { useScroll, useSpring, useTransform } from "framer-motion";
+import useIsomorphicLayoutEffect from "hooks/useIsomorphicLayoutEffect";
+import { RefObject, useState } from "react";
 
-const useZoomOnScroll = (ref: RefObject<HTMLDivElement>) => {};
+const useZoomOnScroll = (ref: RefObject<HTMLDivElement>) => {
+  const [elementTop, setElementTop] = useState(0);
+  const [clientHeight, setClientHeight] = useState(0);
+
+  const { scrollY } = useScroll();
+
+  const initial = Math.max(elementTop, 0);
+  const final = elementTop + clientHeight / 1.4;
+
+  const scaleRange = useTransform(scrollY, [initial, final], [0.4, 1]);
+  const scale = useSpring(scaleRange, { stiffness: 400, damping: 90 });
+
+  useIsomorphicLayoutEffect(() => {
+    const element = ref.current;
+    const onResize = () => {
+      if (!element) return;
+      setElementTop(
+        element.getBoundingClientRect().top + window.scrollY || window.scrollY
+      );
+      setClientHeight(window.innerHeight);
+    };
+    onResize();
+    window.addEventListener("resize", onResize);
+    return () => window.removeEventListener("resize", onResize);
+  }, [ref]);
+
+  return scale;
+};
 
 export default useZoomOnScroll;
